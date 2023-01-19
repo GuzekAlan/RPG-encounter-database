0e4edf0f8a8709e0983841d2c6c80d1e771d02c1
AlanGuzek <alanguzek@gmail.com>
2023-01-19 13:29:59 +0100

    statics

--- rpg_encounter/settings.py
+++ rpg_encounter/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [APPS_DIR.joinpath('static'),]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
