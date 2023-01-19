a949e47f08586fa2d4057b7ca2b0b9ee31619ffd
AlanGuzek <alanguzek@gmail.com>
2023-01-19 13:42:42 +0100

    Spelling mistake

--- rpg_encounter/urls.py
+++ rpg_encounter/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rpg_encounter.encounters import urls as encounters_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(encounters_urls))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
